a44abdc416f40c2b2154951e422fe90cfc348f4c
modargo <modargo@gmail.com>
2021-02-23 20:38:33 -0500

    Fix VoidCalcScorePatch

--- src/main/java/abyss/patches/act4/VoidCalcScorePatch.java
+++ src/main/java/abyss/patches/act4/VoidCalcScorePatch.java
@@ -1,26 +1,28 @@
 package abyss.patches.act4;
 
+import abyss.act.VoidAct;
+import actlikeit.savefields.ElitesSlain;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.dungeons.TheEnding;
 import com.megacrit.cardcrawl.screens.GameOverScreen;
 
+@SpirePatch(
+        clz = GameOverScreen.class,
+        method = "calcScore"
+)
 public class VoidCalcScorePatch {
-    //Forget this for now. Asked Casey to make it much easier for us to do score stuff, and then this would change massively.
-
-    @SpirePatch(
-            clz = GameOverScreen.class,
-            method = "calcScore"
-    )
-    public static class DeathScreenPatch {
-        @SpirePostfixPatch
-        public static int AddUniversalVoidKillBonus(int tmp, boolean isVictory) {
-            if (isVictory && CardCrawlGame.dungeon instanceof TheEnding) {
-                tmp += 250;
-            }
-
-            return tmp;
+    @SpirePostfixPatch
+    public static int AdjustScore(int tmp, boolean isVictory) {
+        //Add 250 points for winning against Universal Void, just like the heart
+        if (isVictory && CardCrawlGame.dungeon instanceof VoidAct) {
+            tmp += 250;
         }
+        //Subtract the points ActLikeIt grants for beating the elite fight in the Void, since vanilla act 4 doesn't give points for killing the elites
+        if (ElitesSlain.getKilledElites().containsKey(VoidAct.ID)) {
+            tmp -= ElitesSlain.getKilledElites().get(VoidAct.ID).kills * VoidAct.ACT_NUM * 10;
+        }
+
+        return tmp;
     }
 }
