0b1bdb30f138f1ac95e6b41c47a38f671708c465
modargo <modargo@gmail.com>
2021-04-07 11:13:37 -0400

    Increase Squirming Horror abysstouched

--- src/main/java/abyss/monsters/normals/SquirmingHorror.java
+++ src/main/java/abyss/monsters/normals/SquirmingHorror.java
@@ -30,8 +30,8 @@ public class SquirmingHorror extends CustomMonster {
     private static final int A2_TENTACLE_WRAP_DAMAGE = 12;
     private static final int TENTACLE_WRAP_ABYSSTOUCHED = 3;
     private static final int A17_TENTACLE_WRAP_ABYSSTOUCHED = 4;
-    private static final int EMBRACE_ABYSSTOUCHED = 6;
-    private static final int A17_EMBRACE_ABYSSTOUCHED = 8;
+    private static final int EMBRACE_ABYSSTOUCHED = 7;
+    private static final int A17_EMBRACE_ABYSSTOUCHED = 9;
     private static final int EMBRACE_SLIMES = 1;
     private static final int A17_EMBRACE_SLIMES = 1;
     private static final int MALLEABLE = 2;
