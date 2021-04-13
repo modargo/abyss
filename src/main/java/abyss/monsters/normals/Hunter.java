c1293d1844fee21bdb5114f0cb0508e04d04fcf9
modargo <modargo@gmail.com>
2021-04-13 14:39:16 -0400

    Switch Hunter's third move to be an attack and buff instead of just a buff

--- src/main/java/abyss/monsters/normals/Hunter.java
+++ src/main/java/abyss/monsters/normals/Hunter.java
@@ -4,6 +4,7 @@ import abyss.Abyss;
 import basemod.abstracts.CustomMonster;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.AnimateFastAttackAction;
+import com.megacrit.cardcrawl.actions.animations.AnimateSlowAttackAction;
 import com.megacrit.cardcrawl.actions.animations.FastShakeAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
@@ -26,13 +27,15 @@ public class Hunter extends CustomMonster {
     private boolean firstMove = true;
     private static final byte RAZOR_SPINES_ATTACK = 1;
     private static final byte SKULK_ATTACK = 2;
-    private static final byte EMPOWER_BUFF = 3;
+    private static final byte EMPOWER_ATTACK = 3;
     private static final int RAZOR_SPINES_DAMAGE = 11;
     private static final int A2_RAZOR_SPINES_DAMAGE = 13;
     private static final int SKULK_DAMAGE = 8;
     private static final int A2_SKULK_DAMAGE = 9;
     private static final int SKULK_PLATED_ARMOR = 2;
     private static final int A7_SKULK_PLATED_ARMOR = 4;
+    private static final int EMPOWER_DAMAGE = 6;
+    private static final int A2_EMPOWER_DAMAGE = 7;
     private static final int EMPOWER_STRENGTH = 1;
     private static final int A17_EMPOWER_STRENGTH = 2;
     private static final int HP_MIN = 53;
@@ -43,6 +46,7 @@ public class Hunter extends CustomMonster {
     private int razorSpinesDamage;
     private int skulkDamage;
     private int skulkPlatedArmor;
+    private int empowerDamage;
     private int empowerStrength;
 
     public Hunter() {
@@ -64,12 +68,15 @@ public class Hunter extends CustomMonster {
         if (AbstractDungeon.ascensionLevel >= 2) {
             this.razorSpinesDamage = A2_RAZOR_SPINES_DAMAGE;
             this.skulkDamage = A2_SKULK_DAMAGE;
+            this.empowerDamage = A2_EMPOWER_DAMAGE;
         } else {
             this.razorSpinesDamage = RAZOR_SPINES_DAMAGE;
             this.skulkDamage = SKULK_DAMAGE;
+            this.empowerDamage = EMPOWER_DAMAGE;
         }
         this.damage.add(new DamageInfo(this, this.razorSpinesDamage));
         this.damage.add(new DamageInfo(this, this.skulkDamage));
+        this.damage.add(new DamageInfo(this, this.empowerDamage));
 
         if (AbstractDungeon.ascensionLevel >= 17) {
             this.empowerStrength = A17_EMPOWER_STRENGTH;
@@ -86,14 +93,16 @@ public class Hunter extends CustomMonster {
         switch (this.nextMove) {
             case RAZOR_SPINES_ATTACK:
                 AbstractDungeon.actionManager.addToBottom(new AnimateFastAttackAction(this));
-                AbstractDungeon.actionManager.addToBottom(new DamageAction(AbstractDungeon.player, this.damage.get(0), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+                AbstractDungeon.actionManager.addToBottom(new DamageAction(AbstractDungeon.player, this.damage.get(0), AbstractGameAction.AttackEffect.SLASH_HEAVY));
                 break;
             case SKULK_ATTACK:
                 AbstractDungeon.actionManager.addToBottom(new AnimateFastAttackAction(this));
-                AbstractDungeon.actionManager.addToBottom(new DamageAction(AbstractDungeon.player, this.damage.get(1), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+                AbstractDungeon.actionManager.addToBottom(new DamageAction(AbstractDungeon.player, this.damage.get(1), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
                 AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(this, this, new PlatedArmorPower(this, this.skulkPlatedArmor), this.skulkPlatedArmor));
                 break;
-            case EMPOWER_BUFF:
+            case EMPOWER_ATTACK:
+                AbstractDungeon.actionManager.addToBottom(new AnimateSlowAttackAction(this));
+                AbstractDungeon.actionManager.addToBottom(new DamageAction(AbstractDungeon.player, this.damage.get(2), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
                 AbstractDungeon.actionManager.addToBottom(new FastShakeAction(this, 0.5F, 0.2F));
                 for (AbstractMonster m : AbstractDungeon.getCurrRoom().monsters.monsters) {
                     if (m == this || !m.isDying) {
@@ -117,14 +126,14 @@ public class Hunter extends CustomMonster {
 
     @Override
     protected void getMove(final int num) {
-        if ((this.firstMove && !this.startWithSkulk) || this.lastMove(EMPOWER_BUFF)) {
+        if ((this.firstMove && !this.startWithSkulk) || this.lastMove(EMPOWER_ATTACK)) {
             this.setMove(MOVES[0], RAZOR_SPINES_ATTACK, Intent.ATTACK, this.razorSpinesDamage);
         }
         else if ((this.firstMove && this.startWithSkulk) || this.lastMove(RAZOR_SPINES_ATTACK)) {
-            this.setMove(MOVES[1], SKULK_ATTACK, Intent.ATTACK_BUFF, this.skulkDamage);
+            this.setMove(MOVES[1], SKULK_ATTACK, Intent.ATTACK_DEFEND, this.skulkDamage);
         }
         else {
-            this.setMove(MOVES[2], EMPOWER_BUFF, Intent.BUFF);
+            this.setMove(MOVES[2], EMPOWER_ATTACK, Intent.ATTACK_BUFF, this.empowerDamage);
         }
     }
 
